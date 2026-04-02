a603c58c695e4211f1e69ebee80870475a0ddd51
Kien Ta <takienww@gmail.com>
2026-04-02 14:47:18 +0700

    in bảng cửu chương

--- python.py
+++ python.py
@@ -1,3 +1,8 @@
 print("hello world")
-for i in range(1, 10):
-    print(i, " ")
+for i in range(2, 10):
+    print(f"Bảng cửu chương {i}")
+    for j in range(1, 11):
+        print(i, "x ", j, "= ", i*j)
+
+        
+
